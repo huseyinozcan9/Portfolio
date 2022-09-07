5f400ccc9d57b4d13a5a7804c14aab0eef96981b
Your Name <hsynozcn009@gmail.com>
2022-09-07 21:09:48 +0300

    Katmanlar oluşturuldu migrate,referans işlemleri uygulandı

---
+++ EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        [Key]
+        public int PortfolioId { get; set; }
+        public int Name { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
